94f352b5903453b808902e314af699ef1a90cb72
test <yuu77471214@icloud.com>
2023-06-29 19:22:20 +0900

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 横井悠希 on 2023/06/29.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
